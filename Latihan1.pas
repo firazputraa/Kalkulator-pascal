4fba942c820afd21815bcedb33c561ee0327b800
Muhammad Firaz Putra <107093059+firazputraa@users.noreply.github.com>
2023-02-07 14:41:23 +0700

    Add files via upload

---
+++ Latihan1.pas
@@ -0,0 +1,11 @@
+program satu;
+uses crt;
+var 
+    a,b:integer;
+begin
+    write('masukan angka pertama: ');
+    readln(a);
+    write('masukan angka kedua: ');
+    readln(b);
+    writeln('hasilnya adalah ', a+b)
+end.
